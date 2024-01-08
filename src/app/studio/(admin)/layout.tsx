58da8e48ea0ead65092c3b1a786b58ecceb0f28c
Aftaza <taftazani17@student.ub.ac.id>
2024-01-08 22:27:12 +0700

    Init header studio

--- src/app/studio/(admin)/layout.tsx
+++ src/app/studio/(admin)/layout.tsx
@@ -1,13 +1,17 @@
 import "@/app/assets/styles/globals.css";
+import { HeaderStudio } from "@/components/header";
 
-export default function StudioLayout({
+export default async function StudioLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
     return (
         <>
-            {children}
+            <HeaderStudio/>
+            <div className="mt-16">
+                {children}
+            </div>
         </>
     );
 }

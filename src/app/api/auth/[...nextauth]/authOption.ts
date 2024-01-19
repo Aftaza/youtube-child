849ce2d034b4ac415145f63e46c146d4167d6371
Aftaza <taftazani17@student.ub.ac.id>
2024-01-19 20:49:37 +0700

    Fixing bug next-auth

---
+++ src/app/api/auth/[...nextauth]/authOption.ts
@@ -0,0 +1,12 @@
+import { AuthOptions } from "next-auth"
+import githubAuth from "next-auth/providers/github"
+
+export const AuthOption: AuthOptions = {
+    providers: [
+        githubAuth({
+            clientId: process.env.GITHUB_CLIENT as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        })
+    ],
+    secret: process.env.NEXTAUTH_SECRET
+}
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
 import NextAuth from "next-auth"
-import githubAuth from "next-auth/providers/github"
-
-export const AuthOption = {
-    providers: [
-        githubAuth({
-            clientId: process.env.GITHUB_CLIENT as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET
-}
+import { AuthOption } from "./authOption"
 
 const handler = NextAuth(AuthOption)
 
